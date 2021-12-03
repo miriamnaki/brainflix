38646dfbb86911a437c33ef9c9757136fcbe48d7
MIRIAM NAKI <nakiyingim@gmail.com>
2021-12-03 12:36:36 -0800

    add-mobile-styling-for-video,header,tags,descriptiion

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import './styles/partials/_globals.scss';
 import { Component } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import VideoDetails from './components/VideoDetails/VideoDetails';
 import videoDetails from './data/video-details.json';
+
 console.log(videoDetails)
 
 
--- src/components/VideoDetails/VideoDetails.js
+++ src/components/VideoDetails/VideoDetails.js
@@ -1,37 +1,62 @@
 import React from 'react';
 import './VideoDetails.scss';
-import Button from '../Button/Button';
-
+import viewsIcon from '../../assets/icons/views.svg'
+import likesIcon from '../../assets/icons/likes.svg'
 
 const VideoDetails = (props) =>  {
   const {video, title, image, channel, timestamp,views,likes,description,comments} = props.selectedVideo
+  let arr = comments.length
+  let postedTimestamp  = new Date(timestamp);
+  let currentDate = postedTimestamp.toLocaleDateString();
+
+   
+  console.log(arr)
   console.log(comments)
   console.log(video)
   return (
     <div className="video">
-      <video className="video__player" controls poster={image} height="200px">
-        <source src={video}></source>
-      </video>
-
-      <h2 className="video__title">{title}</h2>
-
-      <div className="video__info-wrapper">
-        <div className="video__channel-time-wrapper">
-          <p className="video__channel">{channel}</p>
-          <p className="video__time">{timestamp}</p>
-        </div>
-
-        <div className="video__views-likes-wrapper">
-         
-          <button>views</button>
-          <p className="video__views">{views}</p>
-          <button>likes</button>
-          <p className="video__likes">{likes}</p>
-        </div>
-
+      <div className= "video__container">
+        <video className="video__player" controls poster={image}>
+          <source src={video}></source>
+        </video>
       </div>
-          <p>{description}</p>
+
+        <div className="video__details-container">
+          <h2 className="video__title">{title}</h2>
+
+          <hr className="video__divider"></hr>
+
+        <div className="video__info-wrapper">
+          <div className="video__channel-time-wrapper">
+            <p className="video__channel">By {channel}</p>
+            
+            
+            <p className="video__time video__tags">{currentDate}</p>
+          </div>
+
+          <div className="video__views-likes-wrapper">
+            <div className="video__test">
+              <button className="video__views-button video__icon"><img src={viewsIcon} alt="viewsIcon" /></button>
+
+              <p className="video__views-count video__tags">{views}</p>
+            </div>
+
+            <div className="video__test">
+              <button className="video__likes-button video__icon"><img src={likesIcon} alt="viewsIcon" /></button>
+              <p className="video__likes-count video__tags">{likes}</p>
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <hr className="video__divider"></hr>
+          <p className="video__description">{description}</p>
       <div>
+        <div>
+          <p className="video__comment-number">{arr} Comments</p>
+        </div>
 
       </div>
       
@@ -43,7 +68,7 @@ const VideoDetails = (props) =>  {
              
              <div>
 
-               <img src="" alt="avatar-image" />
+               <img src="" alt="placeholder" />
                <p>{comment.name}</p>
                <p>{comment.timestamp}</p>
                <p>{comment.comment}</p>
@@ -53,7 +78,7 @@ const VideoDetails = (props) =>  {
            
         })}
      
-      
+     </div>
     </div>
   );
 }
