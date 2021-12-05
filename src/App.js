008c66dc2b2b8bb60cd79101534776751b1f09d7
MIRIAM NAKI <nakiyingim@gmail.com>
2021-12-04 22:08:16 -0800

    add-video-list-structure

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import { Component } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import VideoDetails from './components/VideoDetails/VideoDetails';
+import VideoList from './components/VideoList/VideoList';
 import videoDetails from './data/video-details.json';
+import videos from './data/videos.json';
 
 console.log(videoDetails)
 
 
  class App extends Component  {
    state = {
-      selectedVideo: videoDetails[0]
+      selectedVideo: videoDetails[0],
+      videoList: videos
    }
 
   render(){
@@ -18,6 +21,7 @@ console.log(videoDetails)
       <>
       <Header/>
       <VideoDetails selectedVideo = {this.state.selectedVideo}/>
+      <VideoList videoList={this.state.videoList}/>
       </>
      
     );
--- src/components/VideoList/VideoList.js
+++ src/components/VideoList/VideoList.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
 
-function VideoList() {
+function VideoList(props) {
+  console.log('these are props', props)
+  const {title, channel,image,src,alt} = props.videoList
   return (
-    <div>
+    <div className="video-list">
+      <h4 className="video-list__header">next video</h4>
+      {props.videoList.map(video => {
+        return (
+          <>
+          <div className="video-list__container">
+
+            <div className="video-list__image-wrapper">
+              <img src={video.image} alt="image" />
+            </div>
+
+            <div className="video-list__title-channel-wrapper">
+              <p className="video-list__title">{video.title}</p>
+              <p className="video-list__channel">{video.channel}</p>
+            </div>
+          </div>
+          </>
+          
+          )
+      })}
       
     </div>
   );
