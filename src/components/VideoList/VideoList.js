d03891387e6de4fb961cf2f6e4bd8d17b5650143
MIRIAM NAKI <nakiyingim@gmail.com>
2021-12-12 16:34:48 -0800

    cleaned up video list page

--- src/components/VideoList/VideoList.js
+++ src/components/VideoList/VideoList.js
@@ -3,33 +3,39 @@ import './VideoList.scss';
 import { Link } from 'react-router-dom';
 
 const  VideoList = (props) => {
-
-
+  // props passed from Home page parent  Component
+  // destructuring out the props
+  const {videoList}  = props
   return (
     <div className="video-list">
       <h4 className="video-list__header">next video</h4>
 
-      {props.videoList.map(video => {
-        return (
-         
-          <Link to={`/videos/${video.id}`} key={video.id} >
-          <div className="video-list__container">
-            <div className="video-list__image-wrapper">
-              <img 
-                className="video-list__image" 
-                src={video.image} 
-                alt="image" 
-                // onClick={() => props.onVideoSelect(video.id)}            
-                />
-            </div>
+      {/* iterate over video list to get individual list properties */}
+      {videoList.map(video => {
+        return ( 
+              <div className="video-list__container" key={video.id}>
+                <div className="video-list__image-wrapper">
+                  {/* image */}
+                  <Link to={`/videos/${video.id}`}>
+                    <img 
+                      className="video-list__image" 
+                      src={video.image} 
+                      alt={video.channel}            
+                      />
+                  </Link>
+                </div>
 
-            <div className="video-list__title-channel-wrapper">
-                <p className="video-list__title">{video.title}</p>
-              <p className="video-list__channel">{video.channel}</p>
-            </div>
-          </div>
-              </Link>
-             
+                <div className="video-list__title-channel-wrapper">
+                  {/* title */}
+                  <Link to={`/videos/${video.id}`} className="video-list__title">
+                    <p className="video-list__title">{video.title}</p>
+                  </Link>
+                  {/* channel */}
+                  <Link to={`/videos/${video.id}`} className="video-list__channel">
+                    <p className="video-list__channel">{video.channel}</p>
+                  </Link>
+                </div>
+              </div>     
           )
       })}    
     </div>
