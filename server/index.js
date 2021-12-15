16cca9d335cc4570a1435273ed582402c8eec6f7
MIRIAM NAKI <nakiyingim@gmail.com>
2021-12-15 07:39:43 -0800

    added cors

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require("cors");
 const videoRoutes = require('./routes/videos');
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.static('./public/images'));
 
